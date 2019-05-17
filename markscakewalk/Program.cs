d64b8a9ba933e0d29bec333e3e13b5123597ed30
Onur Bülbül <onur.bulbul@kaleyazilim.com>
2019-05-17 15:14:22 +0300

    mark's cake walk

---
+++ markscakewalk/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace markscakewalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int[] cakes = new[] { 1, 3, 2 };
+
+            long minKm = findMinKm(n, cakes);
+
+            Console.WriteLine(minKm);
+
+            long findMinKm(int count, int[] arr)
+            {
+                long total = 0;
+
+                Array.Sort(arr);
+                Array.Reverse(arr);
+
+                for (int i = 0; i < n; i++)
+                {
+                    total += ((long)Math.Pow(2, i) * arr[i]);
+                }
+
+                return total;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
