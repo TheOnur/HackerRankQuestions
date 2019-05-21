2fcc09cb2a41cdcd0fa9ee1ce2819570adb7e7c6
Onur Bülbül <onur.bulbul@kaleyazilim.com>
2019-05-21 11:00:55 +0300

    oddoccurrencesinarray added

---
+++ oddoccurrencesinarray/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace oddoccurrencesinarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new[] { 1, 3, 1, 3, 12, 5, 1, 1, 5 };
+
+            int? unpairedItem = MatchItemsInArrayNotEffficent(array);
+            Console.WriteLine(unpairedItem);
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// O(N^2) solution
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        private static int? MatchItemsInArrayNotEffficent(int[] array)
+        {
+            int n = array.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                bool matched = false;
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (array[i] == array[j] && i != j)
+                    {
+                        matched = true;
+                        break;
+                    }
+                }
+
+                if (!matched)
+                {
+                    return array[i];
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// O(N) solution
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        private static int? MatchItemsInArrayEffficent(int[] array)
+        {
+            Array.Sort(array);
+
+            int n = array.Length;
+
+            for (int i = 0; i < n;)
+            {
+                int current = i;
+                int right = current + 1;
+
+                if (right < n && array[current] == array[right])
+                {
+                    i += 2;
+                }
+                else
+                {
+                    return array[current];
+                }
+            }
+
+            return null;
+        }
+    }
+
+}
